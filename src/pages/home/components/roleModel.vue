291ff523837756c794d1e87f28a958853b307744
yynyzy <1601530253@qq.com>
2023-09-08 23:36:50 +0800

    frontend: add rolePic in home

---
+++ src/pages/home/components/roleModel.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="shell">
+    <div class="box" v-for="(item, index) in roles" :key="index">
+      <img :src="item.img"/>
+      <span>{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+// import { ref } from 'vue';s
+
+type RoleType = {
+  img: string
+  name: string
+}
+const roles: RoleType[] = [
+  {
+    name: '小樱',
+    img: ' ./images/home/role1.jpg',
+  },
+  {
+    name: '小樱',
+    img: ' ./images/home/role1.jpg',
+  },
+  {
+    name: '小樱',
+    img: ' ./images/home/role1.jpg',
+  },
+  {
+    name: '小樱',
+    img: ' ./images/home/role1.jpg',
+  },
+  {
+    name: '小樱',
+    img: ' ./images/home/role1.jpg',
+  },
+  {
+    name: '小樱',
+    img: ' ./images/home/role1.jpg',
+  },
+];
+
+</script>
+
+<style lang="less" scoped>
+.shell {
+  width: 100%;
+  height: 750px;
+  display: flex;
+
+
+  .box {
+    flex: 1;
+    overflow: hidden;
+    background-color: #fff;
+    margin: 0 10px;
+    border: 10px #fff;
+    box-shadow: 10px 10px 20px #fff;
+    border-radius: 10px;
+    transition: all 0.5s;
+
+    img {
+      width: 90%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    span {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      height: 15%;
+      font-size: 20px;
+      font-weight: 900;
+    }
+
+    &:hover {
+      flex-basis: 40%;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="home">
-    <Header/>
-    <div>
-      <router-view/>
+    <Header class="header"/>
+    <div class="container">
+      <!-- <router-view/> -->
+      <roleModal class="role-model"/>
     </div>
-    <!-- <div class="title">Home
-    </div> -->
   </div>
 </template>
 
 <script setup lang="ts">
-
+import roleModal from './components/roleModel.vue'
 </script>
 
 <style lang="less" scoped>
 .home {
+  position: relative;
   width: 100vw;
   height: 100vh;
+  .header {
+    position: absolute;
+    top: 0;
+  }
+  .container {
+    height: 100%;
+    padding: 0 100px;
+    background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);
+    overflow:hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .role-model {
+        z-index:1;
+        margin: 100px auto;
+        // background-color: red;
+    }
+  }
 }
 </style>
