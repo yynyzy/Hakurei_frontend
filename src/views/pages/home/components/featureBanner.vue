a9d497e32d55d9714b2826f729280c27d8e16e53
yynyzy <1601530253@qq.com>
2023-12-07 22:09:39 +0800

    frontend: home grid banner update

--- src/views/pages/home/components/featureBanner.vue
+++ src/views/pages/home/components/featureBanner.vue
@@ -2,7 +2,8 @@
   <div class="banner-wrapper">
     <div class="banner">
       <div class="banner-img">
-        <img class="img" src="https://roozen.top/upload/lyhpc2.webp" >
+        <img class="img" src="https://roozen.top/upload/lyhpc2.webp">
+        <span class="img-shadow"></span>
       </div>
       <div class="banner-desc">
         <h2 v-text="i18n.title"></h2>
@@ -24,80 +25,125 @@ const i18n = {
 .banner-wrapper {
   position: relative;
   height: 100%;
-  border-radius: 1rem;
+  border-radius: .3rem;
   background-color: #fff;
   transition: transform .2s ease-in-out;
-
-  .banner {
-    position: relative;
-    top: 0;
-    z-index: 10;
-    display: grid;
-    grid-template-rows: repeat(3, minmax(0, 1fr));
-    overflow: hidden;
-    border-radius: 1rem;
-    .banner-img {
-      position: relative;
-      grid-row: span 1/span 1;
-
-      .img {
-        position: absolute;
-        left: 0;
-        z-index: 20;
-        display: block;
-        height: 120%;
-        width: 100%;
-        background-size: cover;
-        background-repeat: no-repeat;
-        -o-object-fit: cover;
-        object-fit: cover;
-      }
-    }
-
-    .banner-desc {
-      background-color: red;
-      position: relative;
-      z-index: 40;
-      grid-row: span 2/span 2;
-      display: flex;
-      flex-direction: column;
-      padding-left: .3rem;
-      padding-right: .3rem;
-      padding-bottom: .3rem;
-
-      h2 {
-        background-color: aqua;
-        margin-bottom: .3rem;
-        font-size: .5rem;
-        font-weight: 800;
-      }
-
-      article {
-        margin-bottom: .1rem;
-        padding-bottom: .2rem;
-        font-size: .3rem;
-        background-color: aquamarine;
-      }
-    }
-  }
 
   &:hover {
     transform: scale(1.02)
   }
 }
 
-@media (min-width: 1024px) {
-  .banner .banner-img {
-    grid-row: auto;
+.banner {
+  position: relative;
+  display: grid;
+  grid-template-rows: repeat(3, minmax(0, 1fr));
+  overflow: hidden;
+  border-radius: .3rem;
+
+  .banner-img {
+    position: relative;
+    grid-row: span 1/span 1;
+
+    .img {
+      position: absolute;
+      left: 0;
+      display: block;
+      height: 120%;
+      width: 100%;
+      background-size: cover;
+      background-repeat: no-repeat;
+      -o-object-fit: cover;
+      object-fit: cover;
+    }
+
+    .img-shadow {
+      pointer-events: none;
+      position: absolute;
+      left: 0;
+      z-index: 30;
+      height: 120%;
+      width: 100%;
+      opacity: .4;
+    }
+
+    &::after {
+      pointer-events: none;
+      content: "";
+      position: absolute;
+      z-index: 35;
+      left: 71%;
+      top: 0;
+      height: 100%;
+      width: 50%;
+      background: @gradient-cover;
+    }
+  }
+
+  .banner-desc {
+    position: relative;
+    z-index: 40;
+    grid-row: span 2/span 2;
+    display: flex;
+    flex-direction: column;
+    padding-top: .3rem;
+    padding-left: .3rem;
+    padding-right: .3rem;
+    padding-bottom: 1.5rem;
+
+    h2 {
+      margin-bottom: .3rem;
+      font-size: .5rem;
+      font-weight: 800;
+    }
+
+    article {
+      margin-bottom: .1rem;
+      padding-bottom: .2rem;
+      font-size: .3rem;
+      background-color: aquamarine;
+    }
+  }
+}
+
+@media (max-width: 1023px) {
+  .banner {
+    .banner-img {
+      &::after {
+        top: 15%;
+        left: 0;
+        height: 120%;
+        width: 100%;
+        background: @article-cover;
+      }
+    }
   }
 }
 
 @media (min-width: 1024px) {
   .banner {
-    height: 28rem;
+    height: 8rem;
     width: 100%;
     grid-template-columns: repeat(2, minmax(0, 1fr));
     grid-template-rows: none;
-}
+
+    .banner-img {
+
+      .img {
+        height: 8rem;
+        width: 120%;
+      }
+
+      .img-shadow {
+        height: 8rem;
+        width: 120%;
+      }
+    }
+
+    .banner-desc {
+      grid-row: auto;
+      padding: 1rem;
+    }
+  }
 }
 </style>
