68a1453e3cb5fb0f043615cb02ecfeb68c90d80f
yynyzy <1601530253@qq.com>
2024-01-10 21:47:05 +0800

    frontend: add utils useModel

--- src/utils/customVueFun.ts
+++ src/utils/customVueFun.ts
@@ -1,18 +1,45 @@
-// import { customRef } from "vue";
+import { computed, customRef } from "vue";
 
-// export const debounceRef = (value: any, duration: number) => {
-//   let timer;
-//   return customRef((track, trigger) => {
-//     get () {
-//       track();
-//       return value;
-//     },
-//     set (val) {
-//       clearTimeout(timer);
-//       timer = setTimeout(() => {
-//         trigger();
-//         value = val   // 页面上的数据会延迟显示
-//       }, duration);
-//     }
-//   })
-// }
+const debounceRef = (value: any, delay: number) => {
+  let timer: NodeJS.Timeout;
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track();
+        return value;
+      },
+      set(val) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          // 派发更新
+          trigger();
+          value = val;  // 页面上的数据会延迟显示
+        }, delay);
+      }
+    }
+  })
+};
+
+const useModel = (props: any, propName: string, emit: any) => {
+  return computed({
+    get() {
+      return new Proxy(props[propName], {
+        set(obj, name, val) {
+          emit('update:'+propName, {
+            ...obj,
+            [name]: val,
+          });
+          return true;
+        }
+      })
+    },
+    set(val) {
+      emit('update:' + propName, val);
+    }
+  })
+};
+
+export {
+  debounceRef,
+  useModel,
+};
