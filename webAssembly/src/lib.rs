10fed5580bd9cb2daeaaf6149eaefb210c6a9c7e
yynyzy <1601530253@qq.com>
2024-02-16 20:50:03 +0800

    frontend: fix webAssembly

---
+++ webAssembly/src/lib.rs
@@ -0,0 +1,13 @@
+use std::fmt::format;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("webassembly = {}", name));
+}
